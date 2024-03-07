5a3217481a4eebe540d1ebd9cb10d478bace6cd5
Battatt <151041544+Battatt@users.noreply.github.com>
2024-03-07 19:39:03 +0300

    flask-wtf Список профессий

--- main.py
+++ main.py
@@ -21,5 +21,17 @@ def second_index(prof):
     return render_template('prof.html', prof=prof, title="Профессия")
 
 
+@app.route("/list_prof/")
+@app.route("/list_prof/<list>")
+def list_index(list="ul"):
+    l_type = 0 if list == 'ul' else 1 if list == 'ol' else -1
+    professions = [
+        'инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач', 'инженер по терраформированию',
+        'климатолог', 'специалист по радиационной защите', 'астрогеолог', 'гляциолог', 'инженер жизнеобеспечения',
+        'метеоролог', 'оператор марсохода', 'киберинженер', 'штурман', 'пилот дронов'
+    ]
+    return render_template('list_prof.html', list_type=l_type, profs=professions)
+
+
 if __name__ == '__main__':
     app.run(port=5000, host='127.0.0.1')
