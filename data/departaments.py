2f7822b924dbbdeef697c7af3491f7c60b02cd26
Battatt <151041544+Battatt@users.noreply.github.com>
2024-03-08 01:58:22 +0300

    Знакомство с sqlaclhemy Еще одна модель

---
+++ data/departaments.py
@@ -0,0 +1,18 @@
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+
+
+class Departament(SqlAlchemyBase):
+    __tablename__ = 'departaments'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    chief = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    members = sqlalchemy.Column(sqlalchemy.String)
+    email = sqlalchemy.Column(sqlalchemy.String,
+                              index=True, unique=True, nullable=True)
+    relation = sqlalchemy.orm.relationship('User')
+
+    def __repr__(self):
+        return f"<Departament> {self.title}"
