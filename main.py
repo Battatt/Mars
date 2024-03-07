f566521d5b6e22135b9bdf6040769798573eec65
Battatt <151041544+Battatt@users.noreply.github.com>
2024-03-07 19:35:05 +0300

    flask-wtf Тренировки в полёте

--- main.py
+++ main.py
@@ -12,5 +12,14 @@ def index(title="Колонизация Марса"):
     return render_template("base.html", title=title)
 
 
+@app.route("/training/<prof>")
+def second_index(prof):
+    if 'ИНЖЕНЕР' in prof.upper() or 'СТРОИТЕЛ' in prof.upper():
+        prof = 0
+    else:
+        prof = 1
+    return render_template('prof.html', prof=prof, title="Профессия")
+
+
 if __name__ == '__main__':
     app.run(port=5000, host='127.0.0.1')
