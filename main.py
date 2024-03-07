b4289cb655b3f2cf1b2714600280f00d743b321a
Battatt <151041544+Battatt@users.noreply.github.com>
2024-03-07 19:30:35 +0300

    flask-wtf Готовимся к миссии

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route('/')
+@app.route('/<title>')
+@app.route('/index/<title>')
+def index(title="Колонизация Марса"):
+    return render_template("base.html", title=title)
+
+
+if __name__ == '__main__':
+    app.run(port=5000, host='127.0.0.1')
