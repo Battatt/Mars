fd864b1af6330a48c8d33c558999a50790c7ff8b
Battatt <151041544+Battatt@users.noreply.github.com>
2024-03-08 11:23:08 +0300

    Flask-sqlalchemy Добавление авторизации(html-шаблон и модель Loginform)

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from flask import Flask, render_template, redirect, request, make_response
-from forms.login_form_old import LoginForm, SuperSpecialForm
+from flask import Flask, render_template, redirect
 from data import db_session
 from data.jobs import Jobs
 from data.users import User
 from forms.users import RegisterForm
+from flask_login import LoginManager, login_required, login_user, logout_user
+from forms.login_form import LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 @app.route('/')
@@ -54,23 +57,6 @@ def auto_asnwer():
     return render_template("auto_answer.html", **params)
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        f = [form.astro_id.data, form.astro_password.data, form.captain_id.data, form.captain_password.data]
-        return redirect('/success')
-    return render_template('login.html', title='Авторизация', form=form)
-
-
-@app.route("/success", methods=['GET', 'POST'])
-def success():
-    form = SuperSpecialForm()
-    if form.validate_on_submit():
-        return redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-    return render_template("supersubmit.html", title="Yes?", form=form)
-
-
 @app.route("/distribution")
 def rooms(peoples=None):
     if peoples is None:
@@ -107,20 +93,33 @@ def register():
     return render_template('register.html', title='Регистрация', form=form)
 
 
-@app.route("/cookie_test")
-def cookie_test():
-    visits_count = int(request.cookies.get("visits_count", 0))
-    if visits_count:
-        res = make_response(
-            f"Вы пришли на эту страницу {visits_count + 1} раз")
-        res.set_cookie("visits_count", str(visits_count + 1),
-                       max_age=60 * 60 * 24 * 365 * 2)
-    else:
-        res = make_response(
-            "Вы пришли на эту страницу в первый раз за последние 2 года")
-        res.set_cookie("visits_count", '1',
-                       max_age=60 * 60 * 24 * 365 * 2)
-    return res
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
 
 if __name__ == '__main__':
